7480804308820104cd2626538bc3bce6527d81d8
Nandlal <shahnandlal49@gmail.com>
2023-05-10 18:10:38 +0530

    error page implemented

--- frontend/src/Pages/NotFound.js
+++ frontend/src/Pages/NotFound.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import {Box,Text} from "@chakra-ui/react"
 
 function NotFound() {
   return (
-    <>
-        <h1>Error 404: Page not found</h1>
-    </>
+    <Box h={"680px"} bg={'#F7FAFC'} display={'flex'} justifyContent={'center'} alignItems={'center'} color={'gray'}>
+        <Text fontSize={'40px'}> 404 | Not found</Text>
+    </Box>
   )
 }
 
